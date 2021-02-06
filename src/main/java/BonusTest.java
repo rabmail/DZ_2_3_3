f3679ad697335f0ccc9f83736d3c73da9d8eaddc
Dmitriy Dolmatov <Rabmail@mail.ru>
2021-02-06 12:30:09 +0300

    Initial commit

---
+++ src/main/java/BonusTest.java
@@ -0,0 +1,10 @@
+public class BonusTest {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+
+        long amount = 1000_60;
+        boolean registered = true;
+
+        service.calculate(amount, registered);
+    }
+}
